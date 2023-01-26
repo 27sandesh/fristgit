a50ee2c8820b0b1b25656eb1dbc9516ffc42bfa2
27sandesh <kamblesandesh254@gmail.com>
2023-01-26 14:46:24 +0530

    tagname

--- dom.js
+++ dom.js
@@ -5,3 +5,11 @@ for(var i=0; i<items.length; i++)
 {
     items[i].style.fontWeight ='bold';
 }
+
+var it=document.getElementsByTagName("li");
+console.log(it);
+it[1].style.backgroundColor='green';
+for(var i=0; i<it.length; i++)
+{
+    it[i].style.fontWeight ='bold';
+}
