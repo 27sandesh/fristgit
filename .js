f7f325196c1dc1e24cd4a174509878604bf2aafc
27sandesh <kamblesandesh254@gmail.com>
2023-02-01 12:01:38 +0530

    deletebtn

--- .js
+++ .js
@@ -1,1 +1,12 @@
-localStorage.setItem('name','inputtype');
+function savetoLocalStroage(e){
+    e.preventDefault();
+    const name=e.target.name.value;
+    const email=e.target.email.value;
+    const phoneNumber=e.target.phoneNumber.value;
+
+    const obj={
+      name,
+      email,phoneNumber
+    }
+    localStorage.setItem(obj.email, JSON.stringify(obj))
+  }
