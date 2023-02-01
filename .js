3d9f3e2ade44713796bde38be244271308d665a6
27sandesh <kamblesandesh254@gmail.com>
2023-02-01 12:04:00 +0530

    deletebtn

--- .js
+++ .js
@@ -1,12 +1,13 @@
-function savetoLocalStroage(e){
-    e.preventDefault();
-    const name=e.target.name.value;
-    const email=e.target.email.value;
-    const phoneNumber=e.target.phoneNumber.value;
+function savetoLocalStroage(event){
+    event.preventDefault();
+    const name=event.target.name.value;
+    const email=event.target.email.value;
+    const phoneNumber=event.target.phoneNumber.value;
 
     const obj={
       name,
       email,phoneNumber
     }
-    localStorage.setItem(obj.email, JSON.stringify(obj))
+   
   }
+  localStorage.setItem(obj.email, JSON.stringify(obj));
